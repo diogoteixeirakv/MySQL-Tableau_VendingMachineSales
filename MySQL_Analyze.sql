a5a95362238ea8335abe3cbdf5fb9d20daf3ce48
diogoteixeirakv <119432505+diogoteixeirakv@users.noreply.github.com>
2022-12-10 00:45:08 +0000

    Update MySQL_Analyze.sql

--- MySQL_Analyze.sql
+++ MySQL_Analyze.sql
@@ -2,65 +2,66 @@
 SELECT category, count(*) as num_products
 FROM vending_machine.vending_machine_sales
 GROUP BY category -- carbonated=1639, non carbonated=710, food=3427, water=483
-ORDER BY num_products 
-
+ORDER BY num_products DESC
 
 -- ^^ ANALYZE GuttenPlans x1367 ^^ --
-SELECT machine, count(*) as num_products
+SELECT count(*)
 FROM vending_machine.vending_machine_sales
-WHERE machine="GuttenPlans x1367" -- No products:2524
+WHERE machine="GuttenPlans x1367" -- No products sold:2524
 
-SELECT machine, category, count(*) as num_products
+SELECT category, count(*) as num_products
 FROM vending_machine.vending_machine_sales
 WHERE machine="GuttenPlans x1367"
-GROUP BY category -- carbonated=1096, non carbonated=226, food=1202
-ORDER BY num_productS DESC
-*/
+GROUP BY category -- carbonated=1096, non carbonate=226, food=1202
+
 
 -- ^^ ANALYZE EB Public Library x1380 ^^ --
-SELECT machine, count(*) as num_products
+SELECT count(*)
 FROM vending_machine.vending_machine_sales
-WHERE machine="EB Public Library x1380" -- 1833
+WHERE machine="EB Public Library x1380" -- No products sold: 1833
 
-SELECT machine, category, count(*) as num_products
+SELECT category, count(*) as num_products
 FROM vending_machine.vending_machine_sales
 WHERE machine="EB Public Library x1380"
-GROUP BY category -- water=183, carbonated=279, non carbonated=275, food=1096
-ORDER BY num_products DESC
+GROUP BY category -- water=183, carbonated=279, non carbonate=275, food=1096
 
 
 -- ^^ ANALYZE BSQ Mall x1366 - ATT ^^ --
 SELECT count(*)
 FROM vending_machine.vending_machine_sales
-WHERE machine="BSQ Mall x1366 - ATT" -- No products:512
+WHERE machine="BSQ Mall x1366 - ATT" -- No products sold: 512  
 
 SELECT category, count(*) as num_products
 FROM vending_machine.vending_machine_sales
 WHERE machine="BSQ Mall x1366 - ATT"
-GROUP BY category -- water=105, carbonated=106, non carbonated=82, food=219
+GROUP BY category -- water=105, carbonated=106, non carbonate=82, food=219
 
 
 -- ^^ ANALYZE BSQ Mall x1364 - Zales ^^ --
 SELECT count(*)
 FROM vending_machine.vending_machine_sales
-WHERE machine="BSQ Mall x1364 - Zales" -- No products:714
+WHERE machine="BSQ Mall x1364 - Zales" -- No products sold: 714
 
 SELECT category, count(*) as num_products
 FROM vending_machine.vending_machine_sales
 WHERE machine="BSQ Mall x1364 - Zales"
-group by category -- water=181, carbonated=115, non carbonated=99, food=319
+GROUP BY category -- water=181, carbonated=115, non carbonate=99, food=319
 
 
 -- ^^ ANALYZE Earle Asphalt x1371 ^^ --
 SELECT count(*)
 FROM vending_machine.vending_machine_sales
-WHERE machine="Earle Asphalt x1371" -- 676 transações
+WHERE machine="Earle Asphalt x1371" -- No products sold: 676 
 
 SELECT category, count(*) as num_products
 FROM vending_machine.vending_machine_sales
 WHERE machine="Earle Asphalt x1371"
-GROUP BY category -- water=14, carbonated=43, non carbonated=28, food=591
+GROUP BY category -- water=14, carbonated=43, non carbonate=28, food=591
 
-
-
-
+-- ^^ ANALYZE - ALL MACHINES in one table ^^ --
+SELECT machine, category, count(*) as num_products
+FROM vending_machine.vending_machine_sales
+WHERE machine IN ("GuttenPlans x1367","EB Public Library x1380","BSQ Mall x1366 - ATT","BSQ Mall x1364 - Zales","Earle Asphalt x1371")
+GROUP BY machine, category
+HAVING count(*)>0
+ORDER BY machine, num_products DESC
