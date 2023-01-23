41e482c55499383a0b78895398f65fd419a48f94
Diogo <119432505+diogoteixeirakv@users.noreply.github.com>
2023-01-23 17:22:02 +0000

    Update MySQL_Analyze_3.sql

--- MySQL_Analyze_3.sql
+++ MySQL_Analyze_3.sql
@@ -1,9 +1,11 @@
  -- TOTAL SALES ($) - All Machines --
-SELECT SUM(linetotal) as Total_Sales, SUM(transtotal)
+SELECT SUM(linetotal) as Total_Sales
+      ,SUM(transtotal)
 FROM vending_machine.vending_machine_sales -- Total Sales = $12 048.25
 
  -- TOTAL SALES ($) BY MACHINE --
-SELECT machine, SUM(linetotal) AS Total_Sales
+SELECT machine
+      ,SUM(linetotal) AS Total_Sales
 FROM vending_machine.vending_machine_sales
 WHERE machine IN ("GuttenPlans x1367","EB Public Library x1380","BSQ Mall x1366 - ATT","BSQ Mall x1364 - Zales","Earle Asphalt x1371")
 GROUP BY machine
@@ -16,12 +18,3 @@ EB Public Library x1380: $ 3619
 Earle Asphalt x1371: $ 1193.5
 BSQ Mall x1366 - ATT: $	1075.75
 BSQ Mall x1364 - Zales: $ 1342.75
-
-
-
-    
-
-
-
-
-
